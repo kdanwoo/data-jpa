19b3f17c90d2b706372ad3819b69debb2c33e243
kdanwoo <k.danwoo91@gmail.com>
2021-10-09 22:31:48 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기 메서드에 JPQL 쿼리 작성

--- src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
@@ -12,9 +12,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-
-
     //먼저 네임드쿼리를 찾아서 실행하고 (엔티티.해당메서드 명)-> 없으면 메소드쿼리 형태로 사용하기떄문에 바로 아랫줄 주석달아도 돌아간다
     @Query(name="Member.findByUsername") //네임드쿼리 사용시, 네임드쿼리는 실무에서 거의 사용안함
     List<Member> findByUsername(@Param("username") String username);
+
+    // @Query, 리포지토리 메소드에 쿼리 정의하기
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+
 }
