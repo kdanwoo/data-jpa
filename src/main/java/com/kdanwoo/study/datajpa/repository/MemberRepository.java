d063221597be4d20de0a269e100a809ff0f90109
kdanwoo <k.danwoo91@gmail.com>
2021-10-09 18:40:48 +0900

    스프링 데이터 JPA로 NamedQuery 사용

--- src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.kdanwoo.study.datajpa.repository;
 
 import com.kdanwoo.study.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-
+    @Query(name="Member.findByUsername") //네임드쿼리 사용시
+    List<Member> findByUsername(@Param("username") String username);
 }
