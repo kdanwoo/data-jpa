132672f028b0268b7ade973b756e9b91b42f99e3
kdanwoo <k.danwoo91@gmail.com>
2021-10-09 18:00:31 +0900

    공통 인터페이스 적용

--- src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/kdanwoo/study/datajpa/repository/MemberRepository.java
@@ -4,5 +4,11 @@ import com.kdanwoo.study.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+
 }
