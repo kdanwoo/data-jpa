6b94d3838e61fb6044d9abcf741b0fd5b661de28
kdanwoo <k.danwoo91@gmail.com>
2021-10-11 13:17:28 +0900

    스프링 데이터 JPA 사용 스프링 데이터 Auditing 적용 - 등록일, 수정일 스프링 데이터 Auditing 적용 - 등록자, 수정자

--- src/main/java/com/kdanwoo/study/datajpa/DataJpaApplication.java
+++ src/main/java/com/kdanwoo/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,9 @@ package com.kdanwoo.study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
