738f5c9ca383ea5d352aec3b16ee74a777c630f4
kdanwoo <k.danwoo91@gmail.com>
2021-10-11 15:26:04 +0900

    Page 내용을 DTO로 변환하기 엔티티를 API로 노출하면 다양한 문제가 발생한다. 그래서 엔티티를 꼭 DTO로 변환해서 반환해야 한다. Page는 map() 을 지원해서 내부 데이터를 다른 것으로 변경할 수 있다.

--- src/main/java/com/kdanwoo/study/datajpa/controller/MemberController.java
+++ src/main/java/com/kdanwoo/study/datajpa/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.kdanwoo.study.datajpa.controller;
 
+import com.kdanwoo.study.datajpa.dto.MemberDto;
 import com.kdanwoo.study.datajpa.entity.Member;
 import com.kdanwoo.study.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +30,19 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size=5) Pageable pageable) {
+        //엔티티를 API로 노출하면 다양한 문제가 발생
+        //Page는 map() 을 지원해서 내부 데이터를 다른 것으로 변경할 수 있다.
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for (int i =0; i<100; i++){
+            memberRepository.save(new Member("userA"+i,i));
+        }
+
     }
 }
